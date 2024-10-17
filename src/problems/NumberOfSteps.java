2c6a210a39998d60911db7236acda4a3dc8896df
strwbry <namine.liteheart@gmail.com>
2024-10-18 03:47:18 +0700

    feat: number of steps

--- src/problems/NumberOfSteps.java
+++ src/problems/NumberOfSteps.java
@@ -1,4 +1,15 @@
 package problems;
 
 public class NumberOfSteps {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println(numberOfSteps(num));
+    }
+
+    public static int numberOfSteps(int n) {
+        if (n == 0) return 0;
+        // add 1 to count the actual steps
+        else if (n % 2 == 0) return 1 + numberOfSteps(n / 2);
+        else return 1 + numberOfSteps(n - 1);
+    }
 }
