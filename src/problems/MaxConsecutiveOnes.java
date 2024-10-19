472f6c6d1d82f3d3d137c23be12d03f1b85c5f41
strwbry <namine.liteheart@gmail.com>
2024-10-20 01:02:26 +0700

    feat: max consecutive ones

---
+++ src/problems/MaxConsecutiveOnes.java
@@ -0,0 +1,30 @@
+package problems;
+
+public class MaxConsecutiveOnes {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 0, 1, 1, 1};
+        System.out.println("Max consecutive ones: " + findMaxConsecutiveOnes(arr));
+    }
+
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            }
+
+            // change count to 0 when it's not 1
+            else {
+                count = 0;
+            }
+
+            // updates the last one before returning
+            if (count > max) {
+                max = count;
+            }
+        }
+        System.gc();
+        return max;
+    }
+}
