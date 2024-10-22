c280db1c9215c0aa6277327379256305681c61d6
strwbry <namine.liteheart@gmail.com>
2024-10-23 05:03:05 +0700

    feat: check if n and its double exist

---
+++ src/problems/CheckIfNAndItsDoubleExist.java
@@ -0,0 +1,22 @@
+package problems;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class CheckIfNAndItsDoubleExist {
+    public static void main(String[] args) {
+        int[] arr = {10,2,5,3};
+        System.out.println("Contains N and its double? " + checkIfExist(arr));
+    }
+
+    public static boolean checkIfExist(int[] arr) {
+        Set<Integer> set = new HashSet<>();
+        for (int i : arr) {
+            if (set.contains(i * 2) || (i % 2 == 0 && set.contains(i / 2))) {
+                return true;
+            }
+            set.add(i);
+        }
+        return false;
+    }
+}
