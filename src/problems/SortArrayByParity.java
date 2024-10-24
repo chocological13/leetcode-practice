632fa3d442f7ead11b75ed26f0ac864d7728948f
strwbry <namine.liteheart@gmail.com>
2024-10-25 04:13:18 +0700

    feat: sort array by parity

---
+++ src/problems/SortArrayByParity.java
@@ -0,0 +1,26 @@
+package problems;
+
+import java.util.Arrays;
+
+public class SortArrayByParity {
+    public static void main(String[] args) {
+        int[] arr = {3,1,2,4};
+        int[] newArr = sortArrayByParity(arr);
+        System.out.println("Sorted array: " + Arrays.toString(newArr));
+    }
+
+    public static int[] sortArrayByParity(int[] nums) {
+        int writePointer = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                if (i != writePointer) {
+                    int temp = nums[i];
+                    nums[i] = nums[writePointer];
+                    nums[writePointer] = temp;
+                }
+                writePointer++;
+            }
+        }
+        return nums;
+    }
+}
